a5db49168f2fe4c65c0b61cf5a71d100a99b775b
bdestasio33 <bdestasio33@gmail.com>
2020-01-15 20:53:54 -0500

    update Template

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
-import '../assets/css/App.css'
-import React, { Component } from 'react'
+import "../assets/css/App.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "../components/HomePage";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello, Electron!</h1>
-
-        <p>I hope you enjoy using basic-electron-react-boilerplate to start your dev off right!</p>
-      </div>
-    )
+      <Router>
+        <Switch>
+          <Route path="/" component={HomePage} />
+          <Route path="/Home" component={HomePage} />
+        </Switch>
+      </Router>
+    );
   }
 }
 
-export default App
+export default App;
---
+++ src/components/HomePage.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+
+const HomePage = () => {
+  return <h1>Welcome to the template</h1>;
+};
+
+export default HomePage;
